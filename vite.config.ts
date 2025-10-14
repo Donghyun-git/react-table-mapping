16de6bb62f4990a3b39314f210a771465580946f
dhahn <donghyun1113@inzent.com>
2025-10-14 13:55:40 +0900

    Release v1.0.0 1. exclude jsx runtim module build config

--- vite.config.ts
+++ vite.config.ts
@@ -6,83 +6,87 @@ import { visualizer } from 'rollup-plugin-visualizer';
 import { defineConfig } from 'vite';
 import dts from 'vite-plugin-dts';
 
-const isLibraryBuild = process.env.NODE_ENV === 'publish';
-const shouldAnalyze = process.env.ANALYZE === 'true';
+export default defineConfig(({ mode }) => {
+  const isLibraryBuild = mode === 'publish';
+  const shouldAnalyze = process.env.ANALYZE === 'true';
 
-export default defineConfig({
-  plugins: [
-    react({
-      babel: {
-        plugins: isLibraryBuild
-          ? [
-              [
-                'babel-plugin-jsx-remove-data-test-id',
-                {
-                  attributes: ['data-testid'],
-                },
-              ],
-            ]
-          : [],
-      },
-    }),
-    dts({
-      insertTypesEntry: true,
-      exclude: [
-        'e2e/**/*',
-        'stories/**/*',
-        'src/main.tsx',
-        'src/App.tsx',
-        'src/App.css',
-        '**/*.stories.ts',
-        '**/*.stories.tsx',
-        '**/*.test.ts',
-        '**/*.test.tsx',
-      ],
-      tsconfigPath: './tsconfig.lib.json',
-    }),
-    shouldAnalyze &&
-      isLibraryBuild &&
-      visualizer({
-        filename: 'dist/bundle-analysis.html',
-        open: true,
-        gzipSize: true,
-        brotliSize: true,
-        template: 'treemap',
+  return {
+    plugins: [
+      react({
+        jsxRuntime: 'automatic',
+        jsxImportSource: 'react',
+        babel: {
+          plugins: isLibraryBuild
+            ? [
+                [
+                  'babel-plugin-jsx-remove-data-test-id',
+                  {
+                    attributes: ['data-testid'],
+                  },
+                ],
+              ]
+            : [],
+        },
       }),
-  ],
-  build: {
-    lib: {
-      entry: resolve(__dirname, 'src/index.ts'),
-      name: 'ReactTableMapping',
-      formats: ['es', 'cjs'],
-      fileName: (format) => `index.${format === 'es' ? 'mjs' : 'js'}`,
-    },
-    rollupOptions: {
-      external: ['react', 'react-dom', 'react/jsx-runtime', 'uuid', '@radix-ui/react-select', '@radix-ui/react-slot'],
-      output: {
-        globals: {
-          react: 'React',
-          'react-dom': 'ReactDOM',
-          'react/jsx-runtime': 'jsxRuntime',
-          uuid: 'uuid',
-          '@radix-ui/react-select': 'RadixSelect',
-          '@radix-ui/react-slot': 'RadixSlot',
-        },
-        assetFileNames: (assetInfo: PreRenderedAsset): string => {
-          if (assetInfo.name === 'style.css') {
-            return 'react-table-mapping.css';
-          }
-          return assetInfo.name || 'unknown.file';
+      dts({
+        insertTypesEntry: true,
+        exclude: [
+          'e2e/**/*',
+          'stories/**/*',
+          'src/main.tsx',
+          'src/App.tsx',
+          'src/App.css',
+          '**/*.stories.ts',
+          '**/*.stories.tsx',
+          '**/*.test.ts',
+          '**/*.test.tsx',
+        ],
+        tsconfigPath: './tsconfig.lib.json',
+      }),
+      shouldAnalyze &&
+        isLibraryBuild &&
+        visualizer({
+          filename: 'dist/bundle-analysis.html',
+          open: true,
+          gzipSize: true,
+          brotliSize: true,
+          template: 'treemap',
+        }),
+    ],
+    build: {
+      lib: {
+        entry: resolve(__dirname, 'src/index.ts'),
+        name: 'ReactTableMapping',
+        formats: ['es', 'cjs'],
+        fileName: (format) => `index.${format === 'es' ? 'mjs' : 'js'}`,
+      },
+      rollupOptions: {
+        external: ['react', 'react-dom', 'uuid', '@radix-ui/react-select', '@radix-ui/react-slot'],
+        output: {
+          globals: {
+            react: 'React',
+            'react-dom': 'ReactDOM',
+            uuid: 'uuid',
+            '@radix-ui/react-select': 'RadixSelect',
+            '@radix-ui/react-slot': 'RadixSlot',
+          },
+          assetFileNames: (assetInfo: PreRenderedAsset): string => {
+            if (assetInfo.name === 'style.css') {
+              return 'react-table-mapping.css';
+            }
+            return assetInfo.name || 'unknown.file';
+          },
         },
       },
+      sourcemap: true,
+      emptyOutDir: true,
+      target: 'esnext',
     },
-    sourcemap: true,
-    emptyOutDir: true,
-  },
 
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+      },
     },
-  },
+  };
 });
